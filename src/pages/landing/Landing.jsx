1247d1c73549880ca83aa707dbd11af65c841daa
Maria Camila Montilla Orozco <camimontilla20242@gmail.com>
2025-08-25 12:48:24 -0500

    Ajustes a archivo index.html, para dejar su contenido en Landing.jsx

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,18 +1,43 @@
 import { Link } from "react-router-dom";
-import styles from "./Landing.module.css";
+import styles from './Landing.module.css';
 
 export default function Landing() {
+
   return (
-    <>
-      <h1 className={styles.titulo}>Aqui va el LANDING PAGE de SANTIAGO</h1>
-      
+    <div>
+      <header>
+        <nav className={styles.navegacion}>
+          <div className={styles.navegacion_izquierda}>
+            <img src="/Logo.png" alt="Logo" className={styles.logo} />
+            <div className={styles.navegacion_menu}>
+              <p>Inicio</p>
+              <p>Nosotros</p>
+              <p>Instalaciones</p>
+              <p>Contacto</p>
+            </div>
+          </div>
+          <button className={styles.navegacion_boton_realizar_pedido}>
+            Realizar Pedido
+          </button>
+        </nav>
+      </header>
+      <main>
+        <div className={styles.banner}>
+          <img src="/fondo_registro.png" alt="Imagen hamburguesa" />
+          <div className={styles.banner_texto}>
+            <p>DESARROLLAMOS LA FÓRMULA</p>
+            <p>A LA NO TE PODRÁS RESISTIR</p>
+          </div>
+        </div>
+      </main>
+
       <Link to="/cliente">
         <button>Realizar pedido</button>
       </Link>
 
       <Link to={"/registro"}>
-      <button>Registro</button>
+        <button>Registro</button>
       </Link>
-    </>
+    </div>
   );
 }
