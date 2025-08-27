3042d0a8744f431b805c928537ad6045d3647b87
bryanvanegas <bryan.steveen98@hotmail.com>
2025-08-27 15:26:35 -0500

    ADD: Login en react

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -38,6 +38,10 @@ export default function Landing() {
       <Link to={"/registro"}>
         <button>Registro</button>
       </Link>
+
+      <Link to={"/login"}>
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
