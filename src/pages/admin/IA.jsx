57a9f11d7635ae57b39aff8245abf2bc829c46be
Mateo Hurtado <hurtadotorresmateo@gmail.com>
2025-08-28 19:58:28 -0500

    ADD archivos IA.jsx y IA.module.css

---
+++ src/pages/admin/IA.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './IA.module.css'
+
+export default function 
+
+() {
+  return (
+    <div className={styles.cuadroPrincipal}>
+        <div className={styles.cuadroSecundario}>
+            <img className={Styles.logo} src="/Logo.png" alt="Logo" />
+            {/* Titulo del cuadro secundario */}
+            <h1>
+                ESTAS A UN PASO DE GENERAR TU PEDIDO CON IA!
+            </h1>
+            {/* Primer cuadrito donde estan las preguntas */}
+            <div>
+                <p className={Styles.pregunta}>
+                    Cuantas personas comeran hoy ? 
+                </p>
+                <input className={Styles.inputUno} type="number" />
+                <p className={Styles.pregunta}>
+                    Notas adicionales para tu pedido:
+                </p>
+                <input className={Styles.inputDos} type="text" />
+            </div>
+            {/* Segundo cuadrito donde esta el mensaje de rojo */}
+            <div>
+                <h5>
+                    ¡ATENCIÓN ANTOJAD@S! NUESTRA IA PREPARÁ TU PEDIDO BASÁNDOSE EN TUS GUSTOS GUARDADOS EN EL PERFIL. PERFECTO PARA ESOS DÍAS EN LOS QUE TIENES UN ANTOJO MISTERIOSO...¡Y NI TÚ SABES EXACTAMENTE DE QUÉ!
+                </h5>
+            </div>
+            {/* Boton de pagar */}
+            <button className={Styles.boton}>
+                Generar pedido con IA
+            </button>
+        </div>
+    </div>
+  )
+}
