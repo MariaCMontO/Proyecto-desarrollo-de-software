8b6f2d3960c05125ba0ea3f635e6a586a1b8201b
Maria Camila Montilla Orozco <camimontilla20182@gmail.com>
2025-10-04 18:13:43 -0500

    Añadir crossorigin en controller de mensaje

--- back/foodlab/src/main/java/com/foodlab/foodlab/controllers/MensajeController.java
+++ back/foodlab/src/main/java/com/foodlab/foodlab/controllers/MensajeController.java
@@ -11,16 +11,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/mensajes")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
 @Tag(name = "Mensajes", description = "API para la gestión de mensajes")
 public class MensajeController {
 
@@ -30,7 +33,7 @@ public class MensajeController {
     public MensajeController(MensajeService mensajeService) {
         this.mensajeService = mensajeService;
     }
-
+    
     @GetMapping
     @Operation(summary = "Obtener todos los mensajes", description = "Devuelve una lista de todos los mensajes registrados.")
     @ApiResponses(value = {
